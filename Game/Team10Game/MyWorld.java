f266e0f4f19c7a1e7aeb2720d25e7252831c2e54
MillipedeAS <michael_c_ibsen@hotmail.com>
2016-11-09 11:59:29 +0100

    Ups

--- Game/Team10Game/MyWorld.java
+++ Game/Team10Game/MyWorld.java
@@ -9,35 +9,33 @@ import java.util.List;
  * @version (a version number or a date)
  */
 public class MyWorld extends World {
-    public Player thePlayer;
+public Player thePlayer;
 
-    public ScrollController theScroll;
+public ScrollController theScroll;
 
-    private int imageCount = 0;
-
-
-    private GreenfootImage bgImage = new GreenfootImage("o.jpg");
-    
-    public void act() {
-        if (theScroll.getScrolll()) {
-            imageCount -= 5; //(or any other value; small -> slow moving, big -> fast movement)
-            drawBackgroundImage();
-        }
-    }
-
+private int imageCount = 0;
+private GreenfootImage bgImage = new GreenfootImage("o.jpg");
+ 
+   public void act() {
+   if (theScroll.getScrolll()) {
+    imageCount -= 5; //(or any other value; small -> slow moving, big -> fast movement)
+    drawBackgroundImage();
+   }
+   }
+ 
     public void drawBackgroundImage() {
-        if (imageCount > bgImage.getWidth()) {
-            imageCount += bgImage.getWidth();
-        }
-        int temp = imageCount;
-        getBackground().drawImage(bgImage, temp, 0);
-        getBackground().drawImage(bgImage, temp + bgImage.getWidth(), 0);
+    if (imageCount > bgImage.getWidth()) {
+        imageCount += bgImage.getWidth();
     }
-
+    int temp = imageCount;
+    getBackground().drawImage(bgImage, temp, 0);
+    getBackground().drawImage(bgImage, temp + bgImage.getWidth(), 0);
+   }
+    
     public MyWorld()
     {    
-        super(600, 400, 1, false); //The extra 'false' parameter means that the world is unbounded.
-
+        super(3000, 400, 1, false); //The extra 'false' parameter means that the world is unbounded.
+        
         addTiles();
         addEnemies();
         setBackground("o.jpg");
@@ -45,135 +43,104 @@ public class MyWorld extends World {
         addObject(thePlayer, 300, 100); //Creates the player.
         theScroll = new ScrollController();
         addObject(theScroll, 0, -10);
-        prepare();
     }
-
+    
     /**
      * Creates the world.
      */
     public void addTiles()
+
     {
-
-       
-            for(int i=1;i<20;i++)
-            {
-                addObject(new grass_tile(), i*30-15, 415);
-            }
-
-            for(int i=0;i<3;i++)
-            {
-                addObject(new grass_tile(), 560+(i*30), 375);
-            }
-
-            for(int i=0;i<6;i++)
-            {
-                addObject(new grass_tile(), 650+(i*30), 335);
-            }
-
-            for(int i=0;i<5;i++)
-            {
-                addObject(new grass_tile(), 830+(i*30), 375);
-            }
-
-            for(int i=1;i<10;i++)
-            {
-                addObject(new grass_tile(), 950+(i*30), 415);
-            }
-
-            for(int i=1;i<3;i++)
-            {
-                addObject(new grass_tile(), 1220+(i*30), 375);
-            }
-
-            addObject(new grass_tile(), 1310, 335);
-            addObject(new grass_tile(), 1340, 335);
-
-            addObject(new grass_tile(), 1370, 295);
-            addObject(new grass_tile(), 1400, 295);
-
-            addObject(new grass_tile(), 1430, 415);
-            addObject(new grass_tile(), 1460, 415);
-
-            for(int i=1;i<15;i++)
-            {   
-                addObject(new grass_tile(), 1430+(i*30), 415);
-            }
-
-            for(int i=1;i<12;i++)
-            {
-                addObject(new grass_tile(), 1850+(i*30), 375);
-            }
-
-            for(int i=1;i<8;i++)
-            {
-                addObject(new grass_tile(), 2180+(i*30), 415);
-            }
-
-            for(int i=1;i<5;i++)
-            {
-                addObject(new grass_tile(), 2390+(i*30), 375);
-            }
-
-            for(int i=1;i<5;i++)
-            {
-                addObject(new grass_tile(), 2510+(i*30), 335);
-            }
-
-            for(int i=1;i<13;i++)
-            {
-                addObject(new grass_tile(), 2630+(i*30), 375);
-            }
         
-      }
+        
       
-      /**
-         * Creates enemies.
-         */
-        public void addEnemies()
+        for(int i=1;i<20;i++)
         {
-
-        }
-
-        public Player getPlayer()
-        {
-            return thePlayer;
-        }
-
-        public ScrollController getTheScroll()
-        {
-            return theScroll;
+            addObject(new grass_tile(), i*30-15, 415);
         }
         
-
+        for(int i=0;i<3;i++)
+        {
+            addObject(new grass_tile(), 560+(i*30), 375);
+        }
+        
+        for(int i=0;i<6;i++)
+        {
+            addObject(new grass_tile(), 650+(i*30), 335);
+        }
+       
+        for(int i=0;i<5;i++)
+        {
+            addObject(new grass_tile(), 830+(i*30), 375);
+        }
+        
+        for(int i=1;i<10;i++)
+        {
+            addObject(new grass_tile(), 950+(i*30), 415);
+        }
+        
+        for(int i=1;i<3;i++)
+        {
+            addObject(new grass_tile(), 1220+(i*30), 375);
+        }
+           
+        addObject(new grass_tile(), 1310, 335);
+        addObject(new grass_tile(), 1340, 335);
+        
+        addObject(new grass_tile(), 1370, 295);
+        addObject(new grass_tile(), 1400, 295);
+        
+        addObject(new grass_tile(), 1430, 415);
+        addObject(new grass_tile(), 1460, 415);
+        
+        for(int i=1;i<15;i++)
+        {   
+            addObject(new grass_tile(), 1430+(i*30), 415);
+        }
+        
+        for(int i=1;i<12;i++)
+        {
+            addObject(new grass_tile(), 1850+(i*30), 375);
+        }
+        
+        for(int i=1;i<8;i++)
+        {
+            addObject(new grass_tile(), 2180+(i*30), 415);
+        }
+        
+        for(int i=1;i<5;i++)
+        {
+            addObject(new grass_tile(), 2390+(i*30), 375);
+        }
+        
+        for(int i=1;i<5;i++)
+        {
+            addObject(new grass_tile(), 2510+(i*30), 335);
+        }
+    
+         for(int i=1;i<13;i++)
+        {
+            addObject(new grass_tile(), 2630+(i*30), 375);
+        }
+    }
+    
+    
     /**
-     * Prepare the world for the start of the program.
-     * That is: create the initial objects and add them to the world.
+     * Creates enemies.
      */
-    private void prepare()
+    public void addEnemies()
     {
-        Currency currency = new Currency();
-        addObject(currency,388,316);
-        Currency currency2 = new Currency();
-        addObject(currency2,439,309);
-        Currency currency3 = new Currency();
-        addObject(currency3,370,202);
-        Currency currency4 = new Currency();
-        addObject(currency4,146,180);
-        Currency currency5 = new Currency();
-        addObject(currency5,191,216);
-        Currency currency6 = new Currency();
-        addObject(currency6,111,281);
-        Currency currency7 = new Currency();
-        addObject(currency7,156,304);
-        Currency currency8 = new Currency();
-        addObject(currency8,77,198);
-        removeObject(currency7);
-        removeObject(currency6);
-        removeObject(currency8);
-        removeObject(currency4);
-        removeObject(currency5);
-        removeObject(currency3);
-        removeObject(currency);
-        removeObject(currency2);
+        
     }
+    
+    public Player getPlayer()
+    {
+        return thePlayer;
     }
+    
+    
+    public ScrollController getTheScroll()
+    {
+        return theScroll;
+    }
+}
